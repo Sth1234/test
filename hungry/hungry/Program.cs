d758dcf83bd4b679d0de08dfdd5c87b7922b1042
arpitcdc2019 <arpitcdc2019@gmail.com>
2021-08-12 12:40:49 +0530

    first version of hungry code

---
+++ hungry/hungry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hungry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am hungry");
+        }
+    }
+}
